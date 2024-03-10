7ab2f308a45675f2b61ba1ad69ce3ac234a4e478
David Sadler <djs21@ic.ac.uk>
2024-03-10 18:47:53 +0000

    Remove unused data key MOD_ID

--- src/main/java/com/selfdot/cobblemonpolymermegasfabric/DataKeys.java
+++ src/main/java/com/selfdot/cobblemonpolymermegasfabric/DataKeys.java
@@ -2,7 +2,6 @@ package com.selfdot.cobblemonpolymermegasfabric;
 
 public class DataKeys {
 
-    public static final String MOD_ID = "cobblemonmegas";
     public static final String MEGA_SPECIES_FEATURE = "mega";
 
 }
